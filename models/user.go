24551570758e20ae8711537e759f1213ad7c552d
oemcongelados <oemcongelados@gmail.com>
2026-01-20 17:30:40 -0300

    Corrigindo duplicidade de structs

--- models/user.go
+++ models/user.go
@@ -3,9 +3,11 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type User struct {
-	ID       primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	Username string             `json:"username" bson:"username"`
-	// O json:"-" faz com que a senha nunca seja enviada nas respostas da API
-	Password string `json:"password,omitempty" bson:"password"`
-	IsAdmin  bool   `json:"is_Admin" bson:"is_admin"`
+	ID primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+
+	Username string `json:"username" bson:"username"`
+	Password string `json:"password" bson:"password"`
+
+	// A CORREÇÃO IMPORTANTE ESTÁ AQUI:
+	IsAdmin bool `json:"isAdmin" bson:"is_admin"`
 }
