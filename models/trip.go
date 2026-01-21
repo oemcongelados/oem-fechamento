863aba0743248a56e06a01ce02a7e164e2bd4b2a
oemcongelados <oemcongelados@gmail.com>
2026-01-21 12:11:03 -0300

    Corrigindo erro de sintaxe no Model Trip

--- models/trip.go
+++ models/trip.go
@@ -9,13 +9,17 @@ import (
 type Trip struct {
 	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 	UserID    string             `json:"user_id" bson:"user_id"`
-	Approved  bool               `json:"approved" bson:"approved"` // <--- CAMPO NOVO
+	Approved  bool               `json:"approved" bson:"approved"`
 	CreatedAt time.Time          `json:"created_at" bson:"created_at"`
 
 	Route     string `json:"route" bson:"route"`
 	StartDate string `json:"start_date" bson:"start_date"`
-	Driver    string `json:"driver" bson:"driver"`
-	Vehicle   string `json:"vehicle" bson:"vehicle"`
+
+	// Novo campo Data de Chegada
+	EndDate string `json:"end_date" bson:"end_date"`
+
+	Driver  string `json:"driver" bson:"driver"`
+	Vehicle string `json:"vehicle" bson:"vehicle"`
 
 	KmStart float64 `json:"km_start" bson:"km_start"`
 	KmEnd   float64 `json:"km_end" bson:"km_end"`
@@ -24,6 +28,7 @@ type Trip struct {
 	ValueReceived float64 `json:"value_received" bson:"value_received"`
 	ReturnNotes   string  `json:"return_notes" bson:"return_notes"`
 
+	// CORREÇÃO: Removido o espaço extra em bson:"expense_fuel"
 	ExpenseFuel      float64 `json:"expense_fuel" bson:"expense_fuel"`
 	ExpenseDaily     float64 `json:"expense_daily" bson:"expense_daily"`
 	ExpenseAssistant float64 `json:"expense_assistant" bson:"expense_assistant"`
